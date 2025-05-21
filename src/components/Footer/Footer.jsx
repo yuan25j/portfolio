45191078460e04c212ee3971d9c591f76932d698
John Yuan <89277444+yuan25j@users.noreply.github.com>
2025-05-21 15:24:26 -0400

    first

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Made with ❤️ by <span>Shehab Shalan</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/data/portfolio.js
@@ -0,0 +1,93 @@
+const about = {
+  title: "Hi 👋 , I am Shehab Shalan",
+  description:
+    "I am a full stack developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nisi incidunt, repellendus doloribus voluptates debitis porro ut illo eaque repudiandae corporis tenetur unde quae consectetur obcaecati iusto nostrum expedita iste!",
+  github: "https://github.com/shehabshalan",
+  linkedin: "https://www.linkedin.com/in/shehabshalan/",
+  cv: "#",
+};
+
+const projects = {
+  title: "Projects",
+  personalProjects: [
+    {
+      title: "Youtube Clone",
+      short:
+        "Features Firebase authentication, video upload, and video streaming.",
+      description:
+        "it is a video-sharing app built using Firebase for authentication, and data persistence. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nisi incidunt, repellendus doloribus voluptates debitis porro ut illo eaque repudiandae corporis tenetur unde quae consectetur obcaecati iusto nostrum expedita iste! ",
+      technologies: ["#React.js", "#MUI", "#Firebase"],
+      github: "#",
+      demo: "#",
+    },
+    {
+      title: "Next.js Blog",
+      short:
+        "Features JWT authentication, headless CMS, and full CRUD operations.",
+      description:
+        "it is a Next.js blog site built using Strapi (a headless CMS). Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nisi incidunt, repellendus doloribus voluptates debitis porro ut illo eaque repudiandae corporis tenetur unde quae consectetur obcaecati iusto nostrum expedita iste! and also lorem",
+      technologies: ["#Next.js", "#MUI", "#Strapi", "#JWT"],
+      github: "#",
+      demo: "#",
+    },
+    {
+      title: "File Sharing App",
+      short:
+        "Features multi-file upload, sharing links with expiration and document preview.",
+      description:
+        "it is a file sharing app built using the MERN stack. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nisi incidunt, repellendus doloribus voluptates debitis porro ut illo eaque repudiandae corporis tenetur unde quae consectetur obcaecati iusto nostrum expedita iste!",
+      technologies: [
+        "#React.js",
+        "#MUI",
+        "#Express.js",
+        "#Node.js",
+        "#MongoDB",
+      ],
+      github: "#",
+      demo: "#",
+    },
+  ],
+};
+
+const experience = {
+  title: "Experience",
+  experiences: [
+    {
+      title: "Frontend Developer",
+      company: "XYZ Company",
+      duration: "Mar 2021 - Present",
+      description:
+        "I am currently working as a frontend developer at XYZ Company. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nisi incidunt, repellendus doloribus voluptates debitis porro ut illo eaque repudiandae corporis tenetur unde quae consectetur obcaecati iusto nostrum expedita iste! ",
+    },
+  ],
+};
+
+const skills = {
+  title: "Skills",
+  mySkills: [
+    {
+      title: "Languages & Frameworks",
+      skills: [
+        "#HTML",
+        "#CSS",
+        "#JavaScript",
+        "#TypeScript",
+        "#React.js",
+        "#Next.js",
+        "#MUI",
+        "#Node.js",
+        "#Express.js",
+      ],
+    },
+    {
+      title: "Databases",
+      skills: ["#MongoDB", "#PostgreSQL", "#Firebase"],
+    },
+    {
+      title: "Others",
+      skills: ["#Git", "#Jest", "#CI/CD", "#AdobeXD"],
+    },
+  ],
+};
+
+export { about, projects, experience, skills };
